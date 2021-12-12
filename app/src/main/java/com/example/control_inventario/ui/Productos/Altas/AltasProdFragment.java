1a4746ec229a0c47b4538d049d6e43d8e16a3885
ArelyL13 <60859721+ArelyL13@users.noreply.github.com>
2021-12-12 16:20:41 -0600

    altas

--- app/src/main/java/com/example/control_inventario/ui/Productos/Altas/AltasProdFragment.java
+++ app/src/main/java/com/example/control_inventario/ui/Productos/Altas/AltasProdFragment.java
@@ -2,6 +2,7 @@ package com.example.control_inventario.ui.Productos.Altas;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,28 +12,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.control_inventario.R;
+import com.example.control_inventario.databinding.FragmentAltasProdBinding;
 
 public class AltasProdFragment extends Fragment {
 
-    private AltasProdViewModel mViewModel;
+    private AltasProdViewModel altViewModel;
+    private FragmentAltasProdBinding binding;
+    private Button btnguarda;
+    private EditText etID,etProducto, etCantidad, etPrecio;
+    private ImageView miImagenView;
+    private Uri fotoUri;
+
 
     public static AltasProdFragment newInstance() {
         return new AltasProdFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_altas_prod, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+       // return inflater.inflate(R.layout.fragment_altas_prod, container, false);
+        altViewModel= new ViewModelProvider(this).get(AltasProdViewModel.class);
+        binding= FragmentAltasProdBinding.inflate(inflater,container,false);
+        View root= binding.getRoot();
+
+        return root;
+    }
+    private void componentes(View root){
+        EditTextComponent(root);
+        ButtonComponent(root);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AltasProdViewModel.class);
-        // TODO: Use the ViewModel
+    private void ButtonComponent(View root) {
+        btnguarda = root.findViewById(R.id.ALTbtnGuardar);
+        btnguarda.setOnClickListener((View.OnClickListener)this);
     }
+
+    private void EditTextComponent(View root) {
+        etID = root.findViewById(R.id.ALetId);
+        etProducto = root.findViewById(R.id.AlTetProductoNom);
+        etCantidad = root.findViewById(R.id.AlTetCantidad);
+        etPrecio = root.findViewById(R.id.AlTetPrecio);
+    }
+
 
 }
